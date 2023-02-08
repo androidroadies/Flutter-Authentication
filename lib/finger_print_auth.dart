5c5f49f725f82942163594095db3296a84d5a47c
Dipak Skywave <dipak.skywaveinfo@gmail.com>
2023-02-08 10:18:20 +0530

    # move code in main

---
+++ lib/finger_print_auth.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+
+class FingerprintAuth extends StatefulWidget {
+  const FingerprintAuth({Key? key}) : super(key: key);
+
+  @override
+  _FingerprintAuthState createState() => _FingerprintAuthState();
+}
+
+class _FingerprintAuthState extends State<FingerprintAuth> {
+  final auth = LocalAuthentication();
+  String authorized = "Not authorized";
+  bool _canCheckBiometric = false;
+  late List<BiometricType> _availableBiometric;
+
+  Future<void> _authenticate() async {
+    bool authenticated = false;
+
+    try {
+      authenticated = await auth.authenticate(
+        localizedReason: 'Scan your finger to authenticate',
+        options: const AuthenticationOptions(
+            useErrorDialogs: false,
+            ),
+      );
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    setState(() {
+      authorized =
+          authenticated ? "Authorized success" : "Failed to authenticate";
+      print("authorized==>>>$authorized");
+    });
+  }
+
+  Future<void> _checkBiometric() async {
+    bool canCheckBiometric = false;
+
+    try {
+      canCheckBiometric = await auth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      print("_checkBiometric ERROR==>>>$e");
+    }
+    print("_canCheckBiometric==>>>$canCheckBiometric");
+
+    if (!mounted) return;
+
+    setState(() {
+      _canCheckBiometric = canCheckBiometric;
+    });
+  }
+
+  Future _getAvailableBiometric() async {
+    List<BiometricType> availableBiometric = [];
+
+    try {
+      availableBiometric = await auth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      print("availableBiometric ERROR==>>>$e");
+    }
+
+    print("availableBiometric==>>>$availableBiometric");
+
+    setState(() {
+      _availableBiometric = availableBiometric;
+    });
+  }
+
+  @override
+  void initState() {
+    _checkBiometric();
+    _getAvailableBiometric();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey.shade600,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            StreamBuilder<bool>(
+              stream: auth.isDeviceSupported().asStream(),
+              builder: (
+                BuildContext context,
+                AsyncSnapshot<bool> snapshot,
+              ) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.connectionState == ConnectionState.active ||
+                    snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return const Text('Error');
+                  } else if (snapshot.hasData) {
+                    return Text("Device support : ${snapshot.data.toString()}",
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20));
+                  } else {
+                    return const Text('Empty data');
+                  }
+                } else {
+                  return Text('State: ${snapshot.connectionState}');
+                }
+              },
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 50.0),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 15.0),
+                    child: const Text(
+                      "Authenticate using your fingerprint instead of your password",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, height: 1.5),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: Text(
+                      "$authorized",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 15.0),
+                    width: double.infinity,
+                    child: FloatingActionButton(
+                      onPressed: _authenticate,
+                      elevation: 0.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 24.0, vertical: 14.0),
+                        child: Text(
+                          "Authenticate",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
